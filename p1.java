3a997359f8e8a8016f5ee491c531c1f55ef17d71
Prachi16-tech <prachidec16@gmail.com>
2024-08-30 14:34:30 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,10 @@
+//  Name: Prachi Kumari
+//  Enrollment No: 202303103510318
+//  Branch: BTECH CSE
+//  Aim: Write a program that displays Welcome to AMTICS.
+
+public class p1 {
+    public static void main(String[] args){
+        System.out.println("Welcome to AMTICS");
+    }
+}
